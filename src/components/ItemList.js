171f02d17a5bd81d87078c47d97617c816d9dd4d
newrizwankhan <patnul.rizwankhan@gmail.com>
2024-08-30 18:51:13 +0530

    adding itemList.js

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,31 +1,31 @@
-// const ItemsList = ({ items }) => {
-//   console.log(items);
+const ItemsList = ({ items }) => {
+  console.log(items);
 
-//   return (
-//     <div>
-//       <div>
-//         {items.map((item) => (
-//           <div className="border border-b-2">
-//             <div
-//               key={item.card.info.id}
-//               className=" border-gray-200  "
-//             >
-//               <div className="text-left font-medium">
-//                 <span>{item.card.info.name}</span>
-//               </div>
-//               <div className="text-left font-medium">
-//                 <span>₹{item.card.info.price / 100}</span>
-//                 <span>⭐{item.card.info.ratings.aggregatedRating.rating}</span>
-//               </div>
-//             </div>
-//             <div className="text-left  ">
-//               <span>{item.card.info.description}</span>
-//             </div>
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
+  return (
+    <div>
+      <div>
+        {items.map((item) => (
+          <div className="border border-b-2">
+            <div
+              key={item.card.info.id}
+              className=" border-gray-200  "
+            >
+              <div className="text-left font-medium">
+                <span>{item.card.info.name}</span>
+              </div>
+              <div className="text-left font-medium">
+                <span>₹{item.card.info.price / 100}</span>
+                <span>⭐{item.card.info.ratings.aggregatedRating.rating}</span>
+              </div>
+            </div>
+            <div className="text-left  ">
+              <span>{item.card.info.description}</span>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-// export default ItemsList;
+export default ItemsList;
